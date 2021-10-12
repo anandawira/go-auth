9e65bdf4cf7ba995d81eba48b1cde6873e5feeda
Ananda Wiradharma <anandawira754@gmail.com>
2021-10-12 23:24:43 +0700

    add jwt

--- database/connection.go
+++ database/connection.go
@@ -2,10 +2,13 @@ package database
 
 import (
 	"fmt"
+
 	"github.com/anandawira/go-auth/models"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 )
+
+var DB *gorm.DB
 
 func Connect() {
 	dsn := "root:example@tcp(127.0.0.1:3306)/go-auth?charset=utf8mb4&parseTime=True&loc=Local"
@@ -16,6 +19,8 @@ func Connect() {
 		fmt.Println(err)
 		panic("cannot connect to the database")
 	}
+
+	DB = connection
 
 	connection.AutoMigrate(&models.User{})
 }
--- main.go
+++ main.go
@@ -4,11 +4,16 @@ import (
 	"github.com/anandawira/go-auth/database"
 	"github.com/anandawira/go-auth/routes"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	database.Connect()
 	app := fiber.New()
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+	}))
 
 	routes.Setup(app)
 
